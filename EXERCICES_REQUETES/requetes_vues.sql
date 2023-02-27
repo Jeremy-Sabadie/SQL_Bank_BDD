019838b8c60440f091615899274ee6a5e18e632e
cda07saba <jeremy.sabadie@2isa.org>
2023-02-27 10:58:39 +0100

    new exercices instructions added on the new file exercice.

---
+++ EXERCICES_REQUETES/requetes_vues.sql
@@ -0,0 +1,13 @@
+-- OAI – Vues:
+
+-- Q1 - Créer une vue appelée EMP_VU comprenant le numéro d’employé, le nom, le numéro de département de la table EMP. Afficher le contenu de cette vue.
+--=======================================================================================================================
+
+-- Q2 - En utilisant la vue EMP_VU, créer une requête pour afficher les noms et les numéros de département de tous les employés.
+--=======================================================================================================================
+-- Q3 - Créer une vue DEPT_VU montrant tous les départements avec le numéro de département, le nom du département, le nombre d’employés du département. Afficher le contenu de cette vue.
+--=======================================================================================================================
+-- Q4 - Créer une vue SALGRADE_VU montrant tous les employés avec leur numéro, leur nom, leur salaire, leur poste et leur grade. En utilisant la vue SALGRADE_VU, compter le nombre d’employés par grade.
+--=======================================================================================================================
+-- Q5 – Créer une vue DEPT20_VU qui contiendra le numéro, le nom et le numéro de département de tous les employés du département 20.Interdire de modifier la colonne numéro de département à partir de cette vue. Afficher le contenu de la vue DEPT20_VU. Tenter de modifier le numéro de département de l‘employé SMITH (nouvelle valeur 30).
+--=======================================================================================================================
